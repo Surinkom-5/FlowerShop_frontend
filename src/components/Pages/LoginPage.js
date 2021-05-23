aac68453a017dd9d351f8a4c7b63ec58fdefab54
Andrius <andrisb579@gmail.com>
2021-05-23 14:48:18 +0300

    Login and Register forms

--- src/components/Pages/LoginPage.js
+++ src/components/Pages/LoginPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Header1Center } from "../ui/Text";
-import {TextInput,SubmitButton} from "../ui/Form";
+import {TextInput,SubmitButton, LoginForm} from "../ui/Form";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -15,13 +15,7 @@ function LoginPage(props) {
       <Header1Center>Prisijungti</Header1Center>
       <Row className="justify-content-center">
         <Col xs={4}>
-          <form className="login-container">
-            <TextInput type="text" placeholder="El. pašto adresas" />
-            <br/>
-            <TextInput type="password" placeholder="Slaptažodis" />
-            <br/>
-            <SubmitButton>Prisijungti</SubmitButton>
-          </form>
+          <LoginForm/>
         </Col>
       </Row>
     </Container>
